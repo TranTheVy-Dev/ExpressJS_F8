dc391b789bf706b031241fae6bbd15b4fac46abe
DavidTranK4 <vydev050204@gmail.com>
2024-10-25 15:41:18 +0700

    add curd view

--- src/app/controllers/adminController.js
+++ src/app/controllers/adminController.js
@@ -1,9 +1,35 @@
 const products = require("../models/products");
-
+const {multipleMongooesToObject} = require("../../util/mongooes");
 class AdminController {
-  async create(req, res ,next) {
+  //tham so phai theo thu tu req res, next neu khong se bi loi
+  async home(req, res, next) {
+    try {
+      const docs = await products.find({});
+      if (!docs) {
+        res.send("doc not found")
+      }
+      res.render("homeProduct", { docs: multipleMongooesToObject(docs) });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(req, res, next) {
     try {
       res.render("createProduct");
+    } catch (error) {
+      next(error);
+    }
+  }
+  async delete(req, res, next) {
+    try {
+      res.render("deleteProduct");
+    } catch (error) {
+      next(error);
+    }
+  }
+  async update(req, res, next) {
+    try {
+      res.render("updateProduct");
     } catch (error) {
       next(error);
     }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,6 +2,9 @@ const express = require ("express")
 const Router = express.Router()
 const AdminController = require("../app/controllers/adminController")
 
-Router.get('/', AdminController.create)
+Router.get ('/update',AdminController.update)
+Router.get ('/delete',AdminController.delete)
+Router.get ('/create',AdminController.create)
+Router.get('/', AdminController.home)
 
 module.exports = Router
